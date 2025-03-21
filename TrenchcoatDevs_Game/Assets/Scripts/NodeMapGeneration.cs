209a32112ade92a4ccb3f97ad79d574ef02caca6
QihangITB <qihang.li.7e7@itb.cat>
2025-03-21 16:44:45 +0100

    in proccess

--- TrenchcoatDevs_Game/Assets/Scripts/NodeMapGeneration.cs
+++ TrenchcoatDevs_Game/Assets/Scripts/NodeMapGeneration.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class NodeMapGeneration : MonoBehaviour
@@ -11,7 +12,14 @@ public class NodeMapGeneration : MonoBehaviour
     public GameObject Character;
     public GameObject Treasure;
 
+    [Header("Configuration")]
+    public int HealthNodeNum;
+    public int ShopNodeNum;
+    public int CharacterNodeNum;
+    public int TreasureNodeNum;
+
     [Header("Node Levels")]
+    public GameObject TutorialNode;
     public GameObject StartNode;
     public List<GameObject> FirstLevel;
     public List<GameObject> SecondLevel;
@@ -22,11 +30,84 @@ public class NodeMapGeneration : MonoBehaviour
 
     void Start()
     {
-        
+        // Static nodes
+        SetNode(TutorialNode, Battle);
+        SetNode(StartNode, Character);
+        SetNode(BossLevel, Battle);
+
+        // Random nodes
+        GenerateSpecialNode(Health, HealthNodeNum);
+        GenerateSpecialNode(Shop, ShopNodeNum);
+        GenerateSpecialNode(Character, CharacterNodeNum);
+        GenerateSpecialNode(Treasure, TreasureNodeNum);
+
     }
 
     void Update()
     {
         
+    }
+
+    private void GenerateSpecialNode(GameObject node, int generationNum)
+    {
+        int levelNum, nodeNum;
+        levelNum = Random.Range(0, 5);
+        nodeNum = (levelNum == 4 || levelNum == 0) ? Random.Range(1, 4) : Random.Range(0, 5);
+
+        while (generationNum > 0)
+        {
+            if(IsEmptyPosition(levelNum, nodeNum))
+            {
+                switch (levelNum)
+                {
+                    case 0:
+                        SetNode(FirstLevel[nodeNum], node);
+                        break;
+                    case 1:
+                        SetNode(SecondLevel[nodeNum], node);
+                        break;
+                    case 2:
+                        SetNode(ThirdLevel[nodeNum], node);
+                        break;
+                    case 3:
+                        SetNode(FourthLevel[nodeNum], node);
+                        break;
+                    case 4:
+                        SetNode(FifthLevel[nodeNum], node);
+                        break;
+                    default:
+                        Debug.LogError("Invalid value");
+                        break;
+                }
+
+                generationNum--;
+            }
+        }
+    }
+
+    private bool IsEmptyPosition(int LevelNum, int NodeNum)
+    {
+        switch (LevelNum)
+        {
+            case 0:
+                return FirstLevel[NodeNum].transform.childCount == 0;
+            case 1:
+                return SecondLevel[NodeNum].transform.childCount == 0;
+            case 2:
+                return ThirdLevel[NodeNum].transform.childCount == 0;
+            case 3:
+                return FourthLevel[NodeNum].transform.childCount == 0;
+            case 4:
+                return FifthLevel[NodeNum].transform.childCount == 0;
+            default:
+                Debug.LogError("Invalid value");
+                return false;
+        }
+
+    }
+
+    private void SetNode(GameObject node, GameObject prefab)
+    {
+        Instantiate(prefab, node.transform.position, node.transform.rotation, node.transform);
     }
 }
