443ee6b947f337c1ba5dd76626c32080bf3337bf
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2025-04-04 17:45:53 +0200

    Testing colliders.

--- TrenchcoatDevs_Game/Assets/Scripts/NetworkBehaviors/PlayersCursor.cs
+++ TrenchcoatDevs_Game/Assets/Scripts/NetworkBehaviors/PlayersCursor.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using TMPro;
 using Unity.Collections;
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayersCursor : NetworkBehaviour
 {
     NetworkVariable<FixedString512Bytes> playerName = new NetworkVariable<FixedString512Bytes>(default,NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
+    NetworkVariable<uint> _minigameScore = new NetworkVariable<uint>();
     [SerializeField]
     SpriteRenderer _mouseSprite;
     [SerializeField]
     TMP_Text _playerText;
+    [SerializeField]
+    TMP_Text _scoreFeedback;
+
     public override void OnNetworkSpawn()
     {
 
@@ -39,7 +45,31 @@ public class PlayersCursor : NetworkBehaviour
             Vector2 cursorPosition = Input.mousePosition;
             cursorPosition = Camera.main.ScreenToWorldPoint(cursorPosition);
             transform.position = cursorPosition;
+            Camera cam = Camera.main;
+            if (Input.GetMouseButtonDown((int)MouseButton.Left))
+            {
+                if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out RaycastHit hitTarget))
+                {
+                    HitFeedbackTestServerRpc();
+                }
+            }
         }
+    }
+    [ServerRpc]
+    void AddToScoreServerRpc()
+    {
+        _minigameScore.Value++;
+        UpdateScoreClientRpc();
+    }
+    [ClientRpc]
+    void UpdateScoreClientRpc()
+    {
+        _scoreFeedback.text = _minigameScore.Value.ToString();
+    }
+    [ServerRpc]
+    void HitFeedbackTestServerRpc()
+    {
+        Debug.Log($"Player with id {OwnerClientId} has hited something");
     }
     IEnumerator WaitForName()
     {
